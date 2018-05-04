728b9bd40ac95a4443134bdadacf50960bd2ce03
Cloudmusic <BARKTEGH@users.noreply.github.com>
2018-05-04 19:54:07 +0800

    Update DecisinTree.py

--- DecisinTree.py
+++ DecisinTree.py
@@ -41,6 +41,7 @@ def getSubDataset(dataset,colindex,value):
 输入：数据集
 功能：选择最优的特征，以便得到最优的子数据集（可简单的理解为特征在决策树中的先后顺序）
       选出最大信息增益的属性，即第几列 （使用信息增益来选取最优属性）
+      使用该信息增益来选取最优属性会导致结果偏向 属性取值多 的
 输出：最优特征在数据集中的列索引
 '''
 def information_Gain(dataset):
@@ -66,10 +67,83 @@ def information_Gain(dataset):
 输入：数据集
 功能：选择最优的特征，以便得到最优的子数据集（可简单的理解为特征在决策树中的先后顺序）
       选出最大信息增益的属性，即第几列  （使用增益率来选出最优属性）
+      C45决策树 对可取值数目较少的属性有所偏好 
+      /采用启发式来选取  从候选属性中选取信息增益高于平均值的，在从中选择增益率最高的 做为最优属性
 输出：最优特征在数据集中的列索引
 '''
 def C45_gain_ratio(dataset):
-    pass
+    numFeature = len(dataset[0]) - 1
+    EntD = calcShannonEnt(dataset)
+    GainD_ratioMax = 0.0
+    Feature = -1
+    info_Gain = []
+    GainD_ratio = []
+    # 对每个特征计算信息增益
+    for i in range(numFeature):
+        feat_i_values = [example[i] for example in dataset]  # 提取每一列
+        uniqueValues = set(feat_i_values)
+        feat_i_entropy = 0.0
+        IV_i =0.0
+        for value in uniqueValues:
+            subDataset = getSubDataset(dataset, i, value)
+            prob_i = len(subDataset) / float(len(dataset))
+            feat_i_entropy += prob_i * calcShannonEnt(subDataset)
+            IV_i += -prob_i * math.log(prob_i,2)
+        info_Gain_i = EntD - feat_i_entropy
+        if IV_i ==0: IV_i= 0.1  #当IV=0是，会报错,设为一个较小的数
+        Gain_ratio_i = info_Gain_i/IV_i#相比信息增益增加一个固有值IV
+        info_Gain.append(info_Gain_i)
+        GainD_ratio.append(Gain_ratio_i)
+    info_Gain_ave = float(sum(info_Gain)/len(info_Gain))#信息增益的平均
+    #print('信息增益的平均：',info_Gain_ave)
+    for i in range(len(info_Gain)):
+        if info_Gain[i]>= info_Gain_ave:
+            if GainD_ratio[i] > GainD_ratioMax:
+                GainD_ratioMax = GainD_ratio[i]
+                #print(GainD_ratioMax, i)
+                Feature = i
+    return Feature
+'''
+输入：数据集
+功能：选择最优的特征，以便得到最优的子数据集（可简单的理解为特征在决策树中的先后顺序）
+      CART 选出最大信息增益的属性，即第几列  （使用基尼指数来选出最优属性） 
+输出：最优特征在数据集中的列索引
+'''
+def CART(dataset):
+    numFeature = len(dataset[0]) - 1
+    GiniD = 1.0
+    Feature = -1
+    for i in range(numFeature):
+        feat_i_values = [example[i] for example in dataset]  # 提取每一列
+        uniqueValues = set(feat_i_values)
+        Gini_index_i = 0.0
+        for value in uniqueValues:
+            subDataset = getSubDataset(dataset, i, value)
+            prob_i = len(subDataset) / float(len(dataset))
+            Gini_index_i += prob_i * Gini(subDataset)
+        if Gini_index_i < GiniD:
+            GiniD = Gini_index_i
+            Feature = i
+    return Feature
+
+'''
+输入：数据集
+功能：计算数据集的基尼值 
+输出：数据集的基尼值
+'''
+def Gini(dataset):
+    numSample = len(dataset)
+    y = [example[-1] for example in dataset]#提取类别
+    classCount ={}
+    for class_i in y:
+        if class_i not in classCount.keys():
+            classCount[class_i]=0
+        classCount[class_i] +=1
+    counts = [count*count for _,count in classCount.items()]
+    Gini = 1 - float(sum(counts)/(numSample*numSample))
+    #print(Gini)
+    return Gini
+
 '''
 输入：子数据集的类别标签列
 功能：找出该数据集个数最多的类别(叶节点)
@@ -100,7 +174,9 @@ def creatTree(dataset,labels):
     if len(dataset[0])==1:
         return mostClass_node(classlist)
     #找出最好的特征或属性（最大的信息增益的属性）
-    bestFeature = information_Gain(dataset)
+    #bestFeature = information_Gain(dataset) #采用信息增益
+    #bestFeature = C45_gain_ratio(dataset)#采用信息增益率
+    bestFeature = CART(dataset) #采用CART基尼指数
     bestFeatlabel = featurelabels[bestFeature]
     #搭建树结构
     myTree = {bestFeatlabel:{}}
@@ -167,5 +243,5 @@ if __name__ == '__main__':
     labels = ['色泽','根蒂','敲声','纹理','脐部','触感']#python 函数是引用传递，前面函数会删去labels
     trainTree = creatTree(x,featurelabels)
     print(trainTree)
-    classlabel = classify(trainTree,labels,[0,0,0,0,0,0])
+    classlabel = classify(trainTree,labels,[0,1,0,0,1,1])
     print(classlabel)
